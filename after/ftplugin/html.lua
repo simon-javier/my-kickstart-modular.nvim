a31400199416ed118dde466ce83ee45c6841ba58
Simon <168045868+simon-javier@users.noreply.github.com>
2024-12-22 16:37:40 +0800

    change tab behavior in insert mode

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,5 +1,1 @@
--- vim.keymap.set('n', '<leader>rr', ':silent lcd %:p:h<CR>:silent !explorer.exe %<CR>', { desc = 'Open file to browser' })
-
-vim.keymap.set('n', '<leader>rr', function()
-  vim.ui.open(vim.fn.expand '%')
-end, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>rr', ':silent lcd %:p:h<CR>:silent !explorer.exe %<CR>', { desc = 'Open file to browser' })
