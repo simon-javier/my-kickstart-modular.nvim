7eaf2b8183b767f3e1d9c43e920567cc3c2a2f7d
Simon <168045868+simon-javier@users.noreply.github.com>
2024-12-25 17:34:24 +0800

    Change markdown-preview installation method
    It now uses npm to install markdown-preview.

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -2,9 +2,11 @@ return {
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_browser = ''
     end,
+    ft = { 'markdown' },
   },
 }
