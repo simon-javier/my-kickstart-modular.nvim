e8243813844e40ec4ffdfc6f706c4c6334dbaa7d
Simon <168045868+simon-javier@users.noreply.github.com>
2024-12-27 23:41:24 +0800

    Add apa7 snippet for latex

--- lua/kickstart/plugins/blink.lua
+++ lua/kickstart/plugins/blink.lua
@@ -1,7 +1,21 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        -- follow latest release.
+        version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = 'make install_jsregexp',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        config = function()
+          require('luasnip.loaders.from_vscode').lazy_load()
+          require('luasnip.loaders.from_vscode').lazy_load '~/.config/nvim/snippets/'
+          require('luasnip.loaders.from_snipmate').lazy_load { paths = { '~/.config/nvim/snippets' } }
+        end,
+      },
+    },
     version = '*',
     opts = {
       keymap = { preset = 'default' },
