1e59868071c89533046c5ee9d477e436683c7362
simon-javier <simonjavier812+github@gmail.com>
2025-01-09 10:57:27 +0800

    Delete wsl related stuff

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,1 +1,5 @@
-vim.keymap.set('n', '<leader>rr', ':silent lcd %:p:h<CR>:silent !explorer.exe %<CR>', { desc = 'Open file to browser' })
+-- vim.keymap.set('n', '<leader>rr', ':silent lcd %:p:h<CR>:silent !explorer.exe %<CR>', { desc = 'Open file to browser' })
+
+vim.keymap.set('n', '<leader>rr', function()
+  vim.ui.open(vim.fn.expand '%')
+end, { noremap = true, silent = true })
