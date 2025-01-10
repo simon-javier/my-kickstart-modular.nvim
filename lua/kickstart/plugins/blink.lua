4d7a4ff02c59ca69e43b4bd28557f880aed82d98
Simon <168045868+simon-javier@users.noreply.github.com>
2025-01-10 22:56:27 +0800

    "Change blink.cmp menu appearance"

--- lua/kickstart/plugins/blink.lua
+++ lua/kickstart/plugins/blink.lua
@@ -25,6 +25,25 @@ return {
         nerd_font_variant = 'mono',
       },
       signature = { enabled = true },
+
+      snippets = { preset = 'luasnip' },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+
+        menu = {
+          draw = {
+            columns = { { 'kind_icon', 'label', 'label_description', gap = 1 }, { 'kind' } },
+          },
+        },
+      },
     },
   },
 }
